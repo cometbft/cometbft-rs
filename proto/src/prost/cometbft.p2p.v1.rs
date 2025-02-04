38dc6d4e2337d015360e0193032532a7425fcb1f
Anton Kaliaev <anton.kalyaev@gmail.com>
2025-02-04 12:22:37 +0400

    more custom attrs

--- proto/src/prost/cometbft.p2p.v1.rs
+++ proto/src/prost/cometbft.p2p.v1.rs
@@ -1,3 +1,60 @@
+/// NetAddress represents a peer's network address.
+#[derive(::serde::Deserialize, ::serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct NetAddress {
+    #[prost(string, tag = "1")]
+    pub id: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub ip: ::prost::alloc::string::String,
+    #[prost(uint32, tag = "3")]
+    pub port: u32,
+}
+/// ProtocolVersion represents the current p2p protocol version.
+#[derive(::serde::Deserialize, ::serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ProtocolVersion {
+    #[prost(uint64, tag = "1")]
+    pub p2p: u64,
+    #[prost(uint64, tag = "2")]
+    pub block: u64,
+    #[prost(uint64, tag = "3")]
+    pub app: u64,
+}
+/// DefaultNodeInfo is a basic node's information sent to other peers during the
+/// p2p handshake.
+#[derive(::serde::Deserialize, ::serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct DefaultNodeInfo {
+    #[prost(message, optional, tag = "1")]
+    pub protocol_version: ::core::option::Option<ProtocolVersion>,
+    #[prost(string, tag = "2")]
+    pub default_node_id: ::prost::alloc::string::String,
+    #[prost(string, tag = "3")]
+    pub listen_addr: ::prost::alloc::string::String,
+    #[prost(string, tag = "4")]
+    pub network: ::prost::alloc::string::String,
+    #[prost(string, tag = "5")]
+    pub version: ::prost::alloc::string::String,
+    #[prost(bytes = "vec", tag = "6")]
+    pub channels: ::prost::alloc::vec::Vec<u8>,
+    #[prost(string, tag = "7")]
+    pub moniker: ::prost::alloc::string::String,
+    #[prost(message, optional, tag = "8")]
+    pub other: ::core::option::Option<DefaultNodeInfoOther>,
+}
+/// DefaultNodeInfoOther is the misc. application specific data.
+#[derive(::serde::Deserialize, ::serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct DefaultNodeInfoOther {
+    #[prost(string, tag = "1")]
+    pub tx_index: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub rpc_address: ::prost::alloc::string::String,
+}
 /// PacketPing is a request to confirm that the connection is alive.
 #[derive(::serde::Deserialize, ::serde::Serialize)]
 #[allow(clippy::derive_partial_eq_without_eq)]
@@ -55,63 +112,6 @@ pub struct AuthSigMessage {
     pub pub_key: ::core::option::Option<super::super::crypto::v1::PublicKey>,
     #[prost(bytes = "vec", tag = "2")]
     pub sig: ::prost::alloc::vec::Vec<u8>,
-}
-/// NetAddress represents a peer's network address.
-#[derive(::serde::Deserialize, ::serde::Serialize)]
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct NetAddress {
-    #[prost(string, tag = "1")]
-    pub id: ::prost::alloc::string::String,
-    #[prost(string, tag = "2")]
-    pub ip: ::prost::alloc::string::String,
-    #[prost(uint32, tag = "3")]
-    pub port: u32,
-}
-/// ProtocolVersion represents the current p2p protocol version.
-#[derive(::serde::Deserialize, ::serde::Serialize)]
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct ProtocolVersion {
-    #[prost(uint64, tag = "1")]
-    pub p2p: u64,
-    #[prost(uint64, tag = "2")]
-    pub block: u64,
-    #[prost(uint64, tag = "3")]
-    pub app: u64,
-}
-/// DefaultNodeInfo is a basic node's information sent to other peers during the
-/// p2p handshake.
-#[derive(::serde::Deserialize, ::serde::Serialize)]
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct DefaultNodeInfo {
-    #[prost(message, optional, tag = "1")]
-    pub protocol_version: ::core::option::Option<ProtocolVersion>,
-    #[prost(string, tag = "2")]
-    pub default_node_id: ::prost::alloc::string::String,
-    #[prost(string, tag = "3")]
-    pub listen_addr: ::prost::alloc::string::String,
-    #[prost(string, tag = "4")]
-    pub network: ::prost::alloc::string::String,
-    #[prost(string, tag = "5")]
-    pub version: ::prost::alloc::string::String,
-    #[prost(bytes = "vec", tag = "6")]
-    pub channels: ::prost::alloc::vec::Vec<u8>,
-    #[prost(string, tag = "7")]
-    pub moniker: ::prost::alloc::string::String,
-    #[prost(message, optional, tag = "8")]
-    pub other: ::core::option::Option<DefaultNodeInfoOther>,
-}
-/// DefaultNodeInfoOther is the misc. application specific data.
-#[derive(::serde::Deserialize, ::serde::Serialize)]
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct DefaultNodeInfoOther {
-    #[prost(string, tag = "1")]
-    pub tx_index: ::prost::alloc::string::String,
-    #[prost(string, tag = "2")]
-    pub rpc_address: ::prost::alloc::string::String,
 }
 /// PexRequest is a request for peer addresses.
 #[derive(::serde::Deserialize, ::serde::Serialize)]
