bc1a5ac29945b3b46f512bdd8defce74a0904147
Anton Kaliaev <anton.kalyaev@gmail.com>
2025-05-07 18:05:02 +0400

    generate proto conversions using macros

--- cometbft/src/abci/request/apply_snapshot_chunk.rs
+++ cometbft/src/abci/request/apply_snapshot_chunk.rs
@@ -40,10 +40,38 @@ pub struct ApplySnapshotChunk {
 // Protobuf conversions
 // =============================================================================
 
-mod v1 {
+cometbft_old_pb_modules!(abci, {
     use super::ApplySnapshotChunk;
-    use cometbft_proto::abci::v1 as pb;
-    use cometbft_proto::Protobuf;
+
+    impl From<ApplySnapshotChunk> for pb::RequestApplySnapshotChunk {
+        fn from(apply_snapshot_chunk: ApplySnapshotChunk) -> Self {
+            Self {
+                index: apply_snapshot_chunk.index,
+                chunk: apply_snapshot_chunk.chunk,
+                sender: apply_snapshot_chunk.sender,
+            }
+        }
+    }
+
+    impl TryFrom<pb::RequestApplySnapshotChunk> for ApplySnapshotChunk {
+        type Error = crate::Error;
+
+        fn try_from(
+            apply_snapshot_chunk: pb::RequestApplySnapshotChunk,
+        ) -> Result<Self, Self::Error> {
+            Ok(Self {
+                index: apply_snapshot_chunk.index,
+                chunk: apply_snapshot_chunk.chunk.into(),
+                sender: apply_snapshot_chunk.sender,
+            })
+        }
+    }
+
+    impl Protobuf<pb::RequestApplySnapshotChunk> for ApplySnapshotChunk {}
+});
+
+cometbft_pb_modules!(abci, {
+    use super::ApplySnapshotChunk;
 
     impl From<ApplySnapshotChunk> for pb::ApplySnapshotChunkRequest {
         fn from(apply_snapshot_chunk: ApplySnapshotChunk) -> Self {
@@ -70,7 +98,7 @@ mod v1 {
     }
 
     impl Protobuf<pb::ApplySnapshotChunkRequest> for ApplySnapshotChunk {}
-}
+});
 
 mod v1beta1 {
     use super::ApplySnapshotChunk;
--- cometbft/src/proto_macros.rs
+++ cometbft/src/proto_macros.rs
@@ -1,1 +1,39 @@
+//! Macros to facilitate protobuf conversions
 
+macro_rules! cometbft_old_pb_modules {
+    ($name:ident, { $($contents:item)* }) => {
+        mod v0_34 {
+            use cometbft_proto::v0_34::$name as pb;
+            #[allow(unused_imports)]
+            use cometbft_proto::Protobuf;
+
+            $($contents)*
+        }
+        mod v0_37 {
+            use cometbft_proto::v0_37::$name as pb;
+            #[allow(unused_imports)]
+            use cometbft_proto::Protobuf;
+
+            $($contents)*
+        }
+        mod v0_38 {
+            use cometbft_proto::v0_38::$name as pb;
+            #[allow(unused_imports)]
+            use cometbft_proto::Protobuf;
+
+            $($contents)*
+        }
+    };
+}
+
+macro_rules! cometbft_pb_modules {
+    ($name:ident, { $($contents:item)* }) => {
+        mod v1 {
+            use cometbft_proto::v1::$name::v1 as pb;
+            #[allow(unused_imports)]
+            use cometbft_proto::Protobuf;
+
+            $($contents)*
+        }
+    };
+}
