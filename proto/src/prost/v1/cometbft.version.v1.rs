3fc1df10b420f85715c8cdaee1c9a4c99b84a7f9
Anton Kaliaev <anton.kalyaev@gmail.com>
2025-05-07 15:53:56 +0400

    bring back CUSTOM_TYPE_ATTRIBUTES_V_1

--- proto/src/prost/v1/cometbft.version.v1.rs
+++ proto/src/prost/v1/cometbft.version.v1.rs
@@ -12,6 +12,7 @@ pub struct App {
 /// Consensus captures the consensus rules for processing a block in the blockchain,
 /// including all blockchain data structures and the rules of the application's
 /// state transition machine.
+#[derive(::serde::Deserialize, ::serde::Serialize)]
 #[derive(Clone, Copy, PartialEq, ::prost::Message)]
 pub struct Consensus {
     #[prost(uint64, tag = "1")]
