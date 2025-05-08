52399bb9eb5fa86bd2c2db7238be45271a829c11
Anton Kaliaev <anton.kalyaev@gmail.com>
2025-05-08 12:21:28 +0400

    fix CUSTOM_TYPE_ATTRIBUTES_COMMON and CUSTOM_FIELD_ATTRIBUTES

--- proto/src/prost/v0_38/tendermint.version.rs
+++ proto/src/prost/v0_38/tendermint.version.rs
@@ -12,10 +12,13 @@ pub struct App {
 /// Consensus captures the consensus rules for processing a block in the blockchain,
 /// including all blockchain data structures and the rules of the application's
 /// state transition machine.
+#[derive(::serde::Deserialize, ::serde::Serialize)]
 #[derive(Clone, Copy, PartialEq, ::prost::Message)]
 pub struct Consensus {
     #[prost(uint64, tag = "1")]
+    #[serde(with = "crate::serializers::from_str")]
     pub block: u64,
     #[prost(uint64, tag = "2")]
+    #[serde(with = "crate::serializers::from_str", default)]
     pub app: u64,
 }
